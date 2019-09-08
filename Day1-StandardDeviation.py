725cb98b5da07c56f3ac46fa4f0fc63b93b17703
Chaitanya Bhure <chaitubhure@gmail.com>
2019-09-08 12:22:17 -0400

    Create StandardDeviation.py

---
+++ Day1-StandardDeviation.py
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Import library
+import math
+
+# Define functionts
+def mean(data):
+    return sum(data) / len(data)
+
+def stddev(data, size):
+    sum = 0
+    for i in range(size):
+        sum = sum + (data[i] - mean(data)) ** 2
+    return math.sqrt(sum / size)
+
+# Set data
+size = int(input())
+numbers = list(map(int, input().split()))
+
+# Get standard deviation
+print(round(stddev(numbers, size), 1))
